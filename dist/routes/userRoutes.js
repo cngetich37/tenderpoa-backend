f78ba26e90ea9813e84721295961fc690857a891
cngetich37 <cngetich37@gmail.com>
2023-11-25 15:22:09 +0300

    moved the files to a src folder

---
+++ dist/routes/userRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const {
+  registerUser,
+  loginUser,
+  currentUser
+} = require("../controllers/userController");
+const validToken = require("../middleware/validateTokenHandler");
+const router = express.Router();
+
+router.post("/signup", registerUser);
+
+router.post("/login", loginUser);
+
+router.get("/current", validToken, currentUser);
+
+module.exports = router;
