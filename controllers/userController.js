ef3b90051b218204686dc27ec6e65afd19ef8c23
cngetich37 <cngetich37@gmail.com>
2023-12-08 14:33:32 +0300

    updated the user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -132,7 +132,7 @@ const forgotPassword = asyncHandler(async (req, res) => {
       },
     },
     process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "1hr" }
+    { expiresIn: "10m" }
   );
 
   // Create a password reset link with the token
